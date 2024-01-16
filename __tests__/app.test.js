c379e595b884f5fba2bd7dc1ba7e497137e7fff6
Kanithan Alphonse <144917315+Kani2b@users.noreply.github.com>
2024-01-16 20:02:37 +0530

    for trigerring rollback

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -5,7 +5,7 @@ afterAll(() => {
   server.close();
 });
 
-test('It should respond to the GET method', async () => {
+test('It should respond to the GET method with a failure', async () => {
   const response = await request(server).get('/');
-  expect(response.statusCode).toBe(404);
+  expect(response.statusCode).toBe(200); // Change the expected status code to 200 (intentional failure)
 });
