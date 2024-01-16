19a6ae0aad011b92daad840f1acd96fdd617a387
Ubuntu <ubuntu@ip-172-31-17-55.eu-north-1.compute.internal>
2024-01-16 09:58:07 +0000

    updated test files

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,22 +1,12 @@
 const request = require('supertest');
-const app = require('../server');
+const server = require('../server.js');
 
-describe('Test the root path', () => {
-  let server;
+afterAll(() => {
+  server.close();
+});
 
-  beforeAll((done) => {
-    server = app.listen(3000, done);
-  });
-
-  afterAll((done) => {
-    server.close(done);
-  });
-
-  test('It should respond to the GET method', async () => {
-    const response = await request(app).get('/');
-    expect(response.statusCode).toBe(200);
-  });
-
-  // Add more tests as needed
+test('It should respond to the GET method', async () => {
+  const response = await request(server).get('/');
+  expect(response.statusCode).toBe(404);
 });
 
