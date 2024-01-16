4bd35066abeee80f78a0d533331008b9e2a9ef42
Kanithan Alphonse <144917315+Kani2b@users.noreply.github.com>
2024-01-16 20:00:55 +0530

    Update app.test.js

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -5,7 +5,7 @@ afterAll(() => {
   server.close();
 });
 
-test('It should respond to the GET method with a failure', async () => {
+test('It should respond to the GET method', async () => {
   const response = await request(server).get('/');
-  expect(response.statusCode).toBe(200); // Change the expected status code to 200 (intentional failure)
+  expect(response.statusCode).toBe(404);
 });
