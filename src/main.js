8a8fa16a6b316d70d9439bdabc57da42a8148bc7
Kanithan Alphonse <144917315+Kani2b@users.noreply.github.com>
2024-01-16 19:33:49 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,9 +2,6 @@ const React = require('react');
 const ReactDOM = require('react-dom');
 
 const App = () => {
-  // Intentional bug: Accessing a property of an undefined object
-  const buggyValue = undefinedObject.undefinedProperty; // This will throw an error
-
   return (
     React.createElement('div', null,
       React.createElement('h1', null, 'Netflix-like App'),
